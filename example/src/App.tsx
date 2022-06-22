5a938a374c40638e392065c4e86dd89f12cef310
shiftpsh <me@shiftpsh.com>
2022-06-22 15:18:24 +0900

    feat: minimal Lexical example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,31 @@
 import { ThemeProvider } from '@emotion/react'
 import { Editor } from '@solved-ac/content-editor'
-import {
-  Container,
-  SolvedGlobalStyles,
-  solvedThemes, Typo
-} from '@solved-ac/ui-react'
+import { SolvedGlobalStyles, solvedThemes, Typo } from '@solved-ac/ui-react'
 import React from 'react'
 
 const App = () => {
   return (
     <ThemeProvider theme={solvedThemes.light}>
       <SolvedGlobalStyles />
-      <Container>
-        <Typo variant="h1">@solved-ac/content-editor</Typo>
-      </Container>
       <div
         style={{
-          display: 'grid',
-          gridAutoFlow: 'column',
-          gridAutoColumns: 'minmax(0, 1fr)',
-          width: '100%',
           padding: 16,
-          gap: 16,
-          minHeight: 72,
         }}
       >
-        <div>
-          <Editor />
+        <Typo variant="h1">@solved-ac/content-editor</Typo>
+        <div
+          style={{
+            display: 'grid',
+            gridAutoFlow: 'column',
+            gridAutoColumns: 'minmax(0, 1fr)',
+            width: '100%',
+            gap: 16,
+            minHeight: 72,
+          }}
+        >
+          <div>
+            <Editor />
+          </div>
         </div>
       </div>
     </ThemeProvider>
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,5 +1,42 @@
 import React from 'react'
 
+import { $getRoot, $getSelection } from 'lexical'
+
+import { LexicalComposer } from '@lexical/react/LexicalComposer'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
+
+const theme = {
+  // Theme styling goes here
+}
+
+const initialConfig = {
+  namespace: 'MyEditor',
+  theme,
+  onError: console.error,
+}
+
 export const Editor: React.FC = () => {
-  return <div>asdf</div>
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+      />
+      <OnChangePlugin
+        onChange={(state) => {
+          state.read(() => {
+            // Read the contents of the EditorState here.
+            const root = $getRoot()
+            const selection = $getSelection()
+
+            console.log(root, selection)
+          })
+        }}
+      />
+      <HistoryPlugin />
+    </LexicalComposer>
+  )
 }
